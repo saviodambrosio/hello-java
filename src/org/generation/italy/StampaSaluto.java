71230b92823a3c94ee3488bc7b6f4b394f82d8d3
saviodambrosio <saviodambrosio@gmail.com>
2021-11-12 16:35:01 +0100

    test
    test

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,14 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// commento
+		/*
+		 * C:\Program Files (x86)\Common Files\Oracle\Java\javapath
+		 */
+		System.out.println("Ciao Java mi piace molto");
+
+	}
+
+}
